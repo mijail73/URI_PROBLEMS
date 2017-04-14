4fbfc8558cd814299c6439e7f7f65a4dbd0f1c3c
mijail73 <mijail.iso@ciencias.unam.mx>
2017-04-14 17:25:25 -0500

    Distance

---
+++ Uri1016.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Uri1016 {
+
+    public static void main(String[] args) throws IOException {
+	Scanner in = new Scanner(System.in);
+	int entry = in.nextInt();
+	System.out.println((entry * 2) + " minutos");
+    }
+}
