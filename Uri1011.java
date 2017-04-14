8ebce6d4dcc25c647b8d058fe622b134dc17f225
mijail73 <mijail.iso@ciencias.unam.mx>
2017-04-14 01:31:57 -0500

    * Uri1011.java~: Another one (easy)

---
+++ Uri1011.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Uri1011 {
+
+    public static void main(String[] args) throws IOException {
+	Scanner in = new Scanner(System.in);
+	Double pi = 3.14159;
+	Double fraction = 4.0/3;
+	Double radius = in.nextDouble();
+	Double volume = fraction * pi * Math.pow(radius,3);
+	System.out.printf("VOLUME = %.3f", volume);
+    }
+}
