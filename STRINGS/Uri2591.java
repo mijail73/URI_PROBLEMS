b7518037d9d33907eaef238acf008105ec1c8118
mijail73 <mijail.iso@ciencias.unam.mx>
2017-12-05 11:45:38 -0600

    hamehameha mejorado con menos lineas

--- STRINGS/Uri2591.java
+++ STRINGS/Uri2591.java
@@ -4,23 +4,22 @@ public class Uri2591 {
     public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);
 	int cases = in.nextInt();
-	String cadena = "";
-	StringBuilder sb = new StringBuilder();
+	String cadena, sb = "";
 	int aprim = 0;
 	int asegun = 0;
 	int multi = 0;
 	while(cases > 0) {
-	    sb.append("k");
+	    sb += "k";
 	    cadena = in.next();
 	    String[] cadenaArr = cadena.split("k");
 	    aprim = cadenaArr[0].length() - cadenaArr[0].replace("a", "").length();
 	    asegun = cadenaArr[1].length() - cadenaArr[1].replace("a", "").length();
 	    multi = aprim * asegun;
 	    for (int i = 0; i < multi; i++)
-		sb.append("a");
+		sb += "a";
 	    System.out.println(sb);
 	    aprim = asegun = multi = 0;
-	    sb = new StringBuilder();
+	    sb = "";
 	    cases --;
 	}
     }
